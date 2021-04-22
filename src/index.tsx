184b437f634f82165ddad1e3671c32d044e08fb0
suum-lion <suum@likelion.net>
2021-04-22 17:25:48 +0900

    ADD: esbuild-wasm / dynamic fetching and loading npm modules

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,53 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import * as esbuild from "esbuild-wasm";
+import { useCallback, useEffect, useRef, useState } from "react";
+import ReactDom from "react-dom";
+import { unpkgPathPlugin } from "./plugins/unpkg-path-plugin";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const App = () => {
+  const ref = useRef<any>();
+  const [input, setInput] = useState("");
+  const [code, setCode] = useState("");
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  const startService = useCallback(async () => {
+    ref.current = await esbuild.startService({
+      worker: true,
+      wasmURL: "/esbuild.wasm"
+    });
+  }, []);
+
+  const onClick = useCallback(async () => {
+    if (!ref.current) return;
+
+    const result = await ref.current.build({
+      entryPoints: ["index.js"],
+      bundle: true,
+      write: false,
+      plugins: [unpkgPathPlugin()],
+      define: {
+        "process.env.NODE_ENV": '"production"',
+        global: "window"
+      }
+    });
+
+    setCode(result.outputFiles[0].text);
+  }, [input]);
+
+  useEffect(() => {
+    startService();
+  }, []);
+
+  return (
+    <div>
+      <textarea
+        onChange={e => setInput(e.target.value)}
+        defaultValue={input}
+      ></textarea>
+      <div>
+        <button onClick={onClick}>Submit</button>
+      </div>
+      <pre>{code}</pre>
+    </div>
+  );
+};
+
+ReactDom.render(<App />, document.querySelector("#root"));
