b8ea5d5080814dfb68706fbf0c8392f404eb95fa
suum-lion <suum@likelion.net>
2021-04-22 19:10:08 +0900

    ADD: esbuild-wasm / caching and css handling -1

--- src/plugins/unpkg-path-plugin.ts
+++ src/plugins/unpkg-path-plugin.ts
@@ -1,46 +1,30 @@
-import axios from "axios";
 import * as esbuild from "esbuild-wasm";
 
 export const unpkgPathPlugin = () => {
   return {
     name: "unpkg-path-plugin",
     setup(build: esbuild.PluginBuild) {
-      build.onResolve({ filter: /.*/ }, async (args: any) => {
-        console.log("onResolve", args);
-        if (args.path === "index.js")
-          return { path: args.path, namespace: "a" };
-
-        if (args.path.includes("./") || args.path.includes("../"))
-          return {
-            namespace: "a",
-            path: new URL(args.path, `https://unpkg.com${args.resolveDir}/`)
-              .href
-          };
-
+      // Handle root entry file of 'index.js'
+      build.onResolve({ filter: /(^index\.js$)/ }, () => {
         return {
-          namespace: "a",
-          path: `https://unpkg.com/${args.path}`
+          path: "index.js",
+          namespace: "a"
         };
       });
 
-      build.onLoad({ filter: /.*/, namespace: "a" }, async (args: any) => {
-        console.log("onLoad", args);
-
-        if (args.path === "index.js") {
-          return {
-            loader: "jsx",
-            contents: `
-              import React, { useState } from 'react';
-              console.log(React, useState)
-            `
-          };
-        }
-
-        const { data, request } = await axios.get(args.path);
+      // Handle relative paths in a module
+      build.onResolve({ filter: /^\.+\// }, (args: any) => {
         return {
-          loader: "jsx",
-          contents: data,
-          resolveDir: new URL("./", request.responseURL).pathname
+          namespace: "a",
+          path: new URL(args.path, `https://unpkg.com${args.resolveDir}/`).href
+        };
+      });
+
+      // Handle main file of a module
+      build.onResolve({ filter: /.*/ }, (args: any) => {
+        return {
+          namespace: "a",
+          path: `https://unpkg.com/${args.path}`
         };
       });
     }
