22bc005479804ea2470ae72c10d3265483e31da8
suum-lion <suum@likelion.net>
2021-04-26 18:31:25 +0900

    ADD: markdown editor

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,11 @@
 import ReactDOM from "react-dom";
 import CodeCell from "./components/code-cell";
+import TextEditor from "./components/text-editor";
 
 const App = () => {
   return (
     <div>
+      <TextEditor />
       <CodeCell />
     </div>
   );
