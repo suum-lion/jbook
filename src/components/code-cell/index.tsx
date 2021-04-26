2e79eb1a2225d64553ed23ef585eba62a385abf4
suum-lion <suum@likelion.net>
2021-04-26 16:09:04 +0900

    ADD: draggable resizing component

--- src/components/code-cell/index.tsx
+++ src/components/code-cell/index.tsx
@@ -1,26 +1,34 @@
 import "bulmaswatch/superhero/bulmaswatch.min.css";
-import { useCallback, useState } from "react";
+import { useEffect, useState } from "react";
 import bundle from "../../bundler";
 import CodeEditor from "../code-editor";
 import Preview from "../preview";
 import Resizable from "../resizable";
 
 const CodeCell = () => {
-  const [input, setInput] = useState("");
   const [code, setCode] = useState("");
+  const [input, setInput] = useState("");
 
-  const onClick = useCallback(async () => {
-    const output = await bundle(input);
-    setCode(output);
+  useEffect(() => {
+    const timer = setTimeout(async () => {
+      const output = await bundle(input);
+      setCode(output);
+    }, 1000);
+
+    return () => {
+      clearTimeout(timer);
+    };
   }, [input]);
 
   return (
     <Resizable direction="vertical">
-      <div style={{ height: '100%', display: 'flex', flexDirection: 'row' }}>
-        <CodeEditor
-          initialValue="const a = 1;"
-          onChange={value => setInput(value)}
-        />
+      <div style={{ height: "100%", display: "flex", flexDirection: "row" }}>
+        <Resizable direction="horizontal">
+          <CodeEditor
+            initialValue="const a = 1;"
+            onChange={value => setInput(value)}
+          />
+        </Resizable>
         <Preview code={code} />
       </div>
     </Resizable>
--- src/components/preview/index.tsx
+++ src/components/preview/index.tsx
@@ -1,3 +1,4 @@
+import "./index.css";
 import { FC, useEffect, useRef } from "react";
 
 interface PreviewProps {
@@ -6,7 +7,13 @@ interface PreviewProps {
 
 const html = `
     <html>
-      <head></head>
+      <head>
+        <style>
+          html {
+            background-color: white;
+          }
+        </style>
+      </head>
       <body>
         <div id="root"></div>
         <script>
@@ -29,17 +36,21 @@ const Preview: FC<PreviewProps> = ({ code }) => {
 
   useEffect(() => {
     iframe.current.srcdoc = html;
-    iframe.current.contentWindow.postMessage(code, "*");
+    setTimeout(() => {
+      iframe.current.contentWindow.postMessage(code, "*");
+    }, 50);
   }, [code]);
 
   return (
-    <iframe
-      style={{ backgroundColor: "white" }}
-      title="preview"
-      ref={iframe}
-      sandbox="allow-scripts"
-      srcDoc={html}
-    />
+    <div className="preview-wrapper">
+      <iframe
+        style={{ backgroundColor: "white" }}
+        title="preview"
+        ref={iframe}
+        sandbox="allow-scripts"
+        srcDoc={html}
+      />
+    </div>
   );
 };
 
